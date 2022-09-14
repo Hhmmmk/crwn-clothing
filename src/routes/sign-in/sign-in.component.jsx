fa1505343175a11150209c06862f25c84b54f1c9
Hhmmmk <lesly.celzo@gmail.com>
2022-09-14 10:23:57 +0800

    this has the sample for sign in with Googlepopup & GoogleRedirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,43 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+      console.log(response);
+    };
+    getUser();
+  }, []);
+
+  // What was in the lecture
+  // useEffect(async () => {
+  //   const response = await getRedirectResult(auth);
+  // }, []);
+
+  const logGooglePopupUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <div>
-        <h1>Sign up</h1>
-        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <h1>Sign In Page</h1>
+        <button onClick={logGooglePopupUser}>Sign in with Google Popup</button>
+        <button onClick={signInWithGoogleRedirect}>
+          Sign in with Google Redirect
+        </button>
       </div>
     </div>
   );
